7aad02b945000787c977ff5baa5833b370b34756
Ramazan <r.tokoshev@gmail.com>
2021-10-19 08:21:44 +0600

    added a service api, added a component for ramdominal character output, handled errors, and made the spinner loader

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+
+    <div class="loadingio-spinner-spinner-vlruzclnbi">
+      <div class="ldio-8748apnms37"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+      </div>
+    </div>
+  )
+}
+export default Spinner;
--- src/services/gotServices.js
+++ src/services/gotServices.js
@@ -1,4 +1,4 @@
- export default class GotService {
+export default class GotService {
   constructor() {
     this._apiBase = 'https://www.anapioficeandfire.com/api';
   }
@@ -12,10 +12,60 @@
 
     return await res.json();
   };
-  getAllCharacters() {
-    return this.getResource('/characters?page=5&pageSize=10');
+
+  async getAllCharacter() {
+    const result = await this.getResource(`/characters?page=5&pageSize=10`);
+    return result.map(this._transformCharacteh);
   }
-  getCharacters(id) {
-    return this.getResource(`/characters/${id}`);
+  async getCharacter(id) {
+    const character = await this.getResource(`/characters/${id}`);
+    return this._transformCharacteh(character);
   }
+  async getAllHouse() {
+    const houseRes = await this.getResource(`/houses/`);
+    return houseRes.map(this._transformHouse)
+  }
+  async getHouse(id) {
+    const house = await this.getResource(`/houses/${id}/`);
+    return this._transformHouse(house);
+  }
+  async getAllBooks() {
+    const booksRes = await this.getResource(`/books/`);
+    return booksRes.map(this._transformBooks)
+  }
+  async getBooks(id) {
+    const books = await this.getResource(`/books/${id}/`);
+    return this._transformBooks(books);
+  }
+
+  _transformCharacteh(char) {
+    return {
+      name: char.name,
+      gender: char.gender,
+      born: char.born,
+      died: char.died,
+      culture: char.culture
+    }
+  }
+
+  _transformHouse(house) {
+    return {
+      house: house.name,
+      region: house.region,
+      words: house.words,
+      title: house.title,
+      overlord: house.overlord,
+      ancestralWeapons: house.ancestralWeapons
+    }
+  }
+
+  _transformBooks(books) {
+    return {
+      name: books.name,
+      numberOfpages: books.numberOfpages,
+      publiser: books.publiser,
+      released: books.released
+    }
+  }
+
 }
